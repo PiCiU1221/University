2d791674145baacf37b05ba6bc5839c596540291
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2023-01-13 22:06:24 +0100

    Porządkowanie plików

--- Technikum/PHP Projekt 3 - Logowanie/scriptSucc.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('html').css('display', 'none');
-    $('html').fadeIn(1500);
-    $('#usernameTag').css('display', 'none');
-    $('#welcomeTag').css('display', 'none');
-    $('#usernameTag').delay(2000).fadeIn(1500);
-    $('#welcomeTag').delay(900).fadeIn(1500);
-});
